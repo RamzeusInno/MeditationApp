53815706be0aba33b71ca49717eb7c130ac68a83
alexg <alex.gorbanev05@gmail.com>
2024-06-14 15:10:58 +0500

    decoomposed sessionDurationForm into smaller components to reduce nesting

--- lib/widgets/sessionDurationForm.dart
+++ lib/widgets/sessionDurationForm.dart
@@ -21,87 +21,111 @@ class _SessionDurationFormState extends ConsumerState<SessionDurationForm> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text("Time of a period"),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Row(
-                    children: <Widget>[
-                      Flexible(
-                        flex: 1,
-                        child: TextFormField(
-                          initialValue: '0',
-                          decoration: const InputDecoration(
-                            labelText: 'Minutes',
-                          ),
-                          keyboardType: TextInputType.number,
-                          onFieldSubmitted: (value) {
-                            setState(() {
-                              _minutes = int.parse(value);
-                            });
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Flexible(
-                        flex: 1,
-                        child: TextFormField(
-                          initialValue: '10',
-                          decoration: const InputDecoration(  
-                            labelText: 'Seconds',
-                          ),
-                          keyboardType: TextInputType.number,
-                          onFieldSubmitted: (value) {
-                            setState(() {
-                              _seconds = int.parse(value);
-                            });
-                          },
-                        ),
-                      ),
-                  ]
-                  ),
-                ),
-                Flexible(
-                  flex: 1,
-                  child: TextFormField(
-                      initialValue: '3',
-                      decoration: const InputDecoration(
-                        labelText: 'Number of repetitions',
-                      ),
-                      keyboardType: TextInputType.number,
-                      onFieldSubmitted: (value) {
-                        setState(() {
-                          _repetitions = int.parse(value);
-                        });
-                      },
-                    ),
-                ),
-                  const SizedBox(width: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState?.validate() == true) {
-                          Session session = Session(
-                            _minutes,
-                            _seconds,
-                            _repetitions,
-                          );
-
-                          ref.read(upcomingSessionNotifierProvider.notifier).addSession(session);
-                        }
-                      },
-                      child: Text('Submit'),
-                    ),
-                  ),
-                ],
-            ),
-          ),
+          child: sessionPreferences(),
         ),
       ),
+    );
+  }
+
+  Expanded sessionPreferences() {
+    return Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Time of a period"),
+              periodDuration(),
+              Flexible(
+                flex: 1,
+                child: repetitionsForm(),
+              ),
+                const SizedBox(width: 10),
+                submitButton(),
+              ],
+          ),
+        );
+  }
+
+  TextFormField repetitionsForm() {
+    return TextFormField(
+      initialValue: '3',
+      decoration: const InputDecoration(
+        labelText: 'Number of repetitions',
+      ),
+      keyboardType: TextInputType.number,
+      onFieldSubmitted: (value) {
+        setState(() {
+          _repetitions = int.parse(value);
+        });
+      },
+    );
+  }
+
+  Padding submitButton() {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState?.validate() == true) {
+            Session session = Session(
+              _minutes,
+              _seconds,
+              _repetitions,
+            );
+
+            ref.read(upcomingSessionNotifierProvider.notifier).addSession(session);
+          }
+        },
+        child: Text('Submit'),
+      ),
+    );
+  }
+
+  Padding periodDuration() {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: minutesForm(),
+          ),
+          const SizedBox(width: 10),
+          Flexible(
+            flex: 1,
+            child: secondsForm(),
+          ),
+      ]
+      ),
+    );
+  }
+
+  TextFormField secondsForm() {
+    return TextFormField(
+      initialValue: '10',
+      decoration: const InputDecoration(  
+        labelText: 'Seconds',
+      ),
+      keyboardType: TextInputType.number,
+      onFieldSubmitted: (value) {
+        setState(() {
+          _seconds = int.parse(value);
+        });
+      },
+    );
+  }
+
+  TextFormField minutesForm() {
+    return TextFormField(
+      initialValue: '0',
+      decoration: const InputDecoration(
+        labelText: 'Minutes',
+      ),
+      keyboardType: TextInputType.number,
+      onFieldSubmitted: (value) {
+        setState(() {
+          _minutes = int.parse(value);
+        });
+      },
     );
   }
 }
