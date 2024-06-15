956daabe06f6c8b910ee4194692be130ffbf1c93
alexg <alex.gorbanev05@gmail.com>
2024-06-16 04:17:39 +0500

    added timer and change of the background color

--- lib/screens/sessionScreen.dart
+++ lib/screens/sessionScreen.dart
@@ -1,8 +1,9 @@
+import 'package:contrast_shower_appplication/providers/colorProvider.dart';
 import 'package:contrast_shower_appplication/providers/selectedSessionProvider.dart';
-import 'package:contrast_shower_appplication/session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:contrast_shower_appplication/widgets/timerWidget.dart';
+import 'dart:async';
 
 
 class SessionScreen extends ConsumerStatefulWidget {
@@ -13,17 +14,25 @@ class SessionScreen extends ConsumerStatefulWidget {
 }
 
 class _SessionscreenState extends ConsumerState<SessionScreen> {
+  Timer? timer;
+
   @override
   Widget build(BuildContext context) {
-    final session = ref.watch(selectedSessionNotifierProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Contrast shower session"),
-        backgroundColor: Colors.blue[900],
-      ),
-      body: const Center(
-        child: Timerwidget(),
-      ),
+    return Consumer(
+      builder: (context, ref, child) {
+        Color colorNotifier = ref.watch(colorNotifierProvider);
+        
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Contrast shower session"),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: Container(
+            color: colorNotifier,
+            child: const Timerwidget(),
+          ),
+        );
+      },
     );
   }
 }
--- lib/widgets/timerWidget.dart
+++ lib/widgets/timerWidget.dart
@@ -1,3 +1,4 @@
+import 'package:contrast_shower_appplication/providers/colorProvider.dart';
 import 'package:contrast_shower_appplication/providers/selectedSessionProvider.dart';
 import 'package:contrast_shower_appplication/session.dart';
 import 'package:flutter/material.dart';
@@ -18,8 +19,31 @@ class _TimerwidgetState extends ConsumerState<Timerwidget> {
   Timer? timer;
 
   void startTimer() {
+    final Session session = ref.watch(selectedSessionNotifierProvider);
+    final colorNotifier = ref.read(colorNotifierProvider.notifier);
+
     timer = Timer.periodic(const Duration(seconds: 1), (_){
-      setState(() => sessionDuration -= const Duration(seconds: 1));
+      setState(() {
+        if (sessionDuration.inSeconds <= 0) {
+          colorNotifier.setColor(Colors.white);
+          timer?.cancel();
+          return;
+        }
+
+        int dif = session.getSessionDuration().inSeconds - sessionDuration.inSeconds;
+        int periodDuration = session.getPeriodDuration().inSeconds;
+        if (dif % periodDuration == 0) {
+          print("$dif - $periodDuration - ${dif % (periodDuration * 2)}");
+
+          if (dif % (2 * periodDuration) == 0) {
+            colorNotifier.setColor(Colors.blue);
+          } else {
+            colorNotifier.setColor(Colors.red);
+          }
+        }
+
+        sessionDuration -= const Duration(seconds: 1);
+      });
     }
     );
   }
@@ -27,11 +51,14 @@ class _TimerwidgetState extends ConsumerState<Timerwidget> {
 
   @override
   Widget build(BuildContext context) {
+    final Session session = ref.watch(selectedSessionNotifierProvider);
+
     if (!sessionDurationInitialized) {
-      final Session session = ref.watch(selectedSessionNotifierProvider);
       sessionDuration = session.getSessionDuration();
       sessionDurationInitialized = true;
     }
+
+    
 
     return Column(
       children: [
@@ -42,7 +69,25 @@ class _TimerwidgetState extends ConsumerState<Timerwidget> {
   }
  
   Widget startTimerButton() {
-    return ElevatedButton(
+    final bool timerIsRunning = timer != null && timer!.isActive;
+    final bool timerIsCompleted = sessionDuration.inSeconds <= 0;
+
+    return timerIsRunning || !timerIsCompleted && timer != null ?
+     Row(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            if (timerIsRunning) { 
+              setState(() => timer?.cancel());
+            } else {
+              startTimer();
+            }
+          },
+           child: timerIsRunning ? const Text('Pause') : const Text('Resume'),
+          ),
+      ],
+     ):
+     ElevatedButton(
       onPressed: () => startTimer(),
        child: const Text('Start timer'),
       );
