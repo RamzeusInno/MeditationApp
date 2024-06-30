ccc0a8f8ff22360f40ec448c3cb9fe13411761ef
alexg <alex.gorbanev05@gmail.com>
2024-06-30 05:09:15 +0500

    fix: made audio files work in accordance with the buttons

--- lib/screens/audio_player.dart
+++ lib/screens/audio_player.dart
@@ -7,11 +7,23 @@ class AudioPlayerManager {
     _audioPlayer = AudioPlayer();
   }
 
-Future<void> playAudio(String selectedSongPath) async {
-  await _audioPlayer.setUrl(selectedSongPath);
-  await _audioPlayer.play();
-}
+  Future<void> playAudio(String selectedSongPath) async {
+    await _audioPlayer.setUrl(selectedSongPath);
+    await _audioPlayer.play();
+  }
 
+  Future<void> pauseAudio() async {
+    await _audioPlayer.pause();
+  }
+
+  Future<void> resumeAudio() async {
+    if (_audioPlayer.playing) {
+      await _audioPlayer.seek(_audioPlayer.position);
+    } else {
+      await _audioPlayer.play();
+    }
+
+  }
 
   Future<void> stopAudio() async {
     await _audioPlayer.stop();
